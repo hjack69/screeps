6d1f7bc180ddb32d14701cd6e811bd17aff8db9d
Jack <jackbrobst@nau.edu>
2016-09-23 19:08:10 -0700

    Big overhaul of the main functionality to accommodate control of multiple rooms.
    - Energy Queue, Spawn Queue and associated information is now stored under current_room/memory/current_phase
    - Each room now requires a phase element indicating the name of the current phase
    - Global Memory requires an element myRooms indicating which rooms are currently controlled
    - Global Memory requires an element doRunThings indicating whether to run main loop or not (prevents the console from clogging so you can set the right memory values)

--- role.wallMaintainer.js
+++ role.wallMaintainer.js
@@ -5,8 +5,8 @@ var role = {
         for (var i in Memory.myRooms) { var r = Memory.myRooms[i];
             out[r] = [
                 Game.rooms[r].find(FIND_STRUCTURES, {filter:(s)=>{return (s.structureType==STRUCTURE_WALL||s.structureType==STRUCTURE_RAMPART) && s.hits<500000}}),
-                Game.rooms[r].find(FIND_STRUCTURES, {filter:(s)=>{return (s.structureType==STRUCTURE_WALL|| s.structureType==STRUCTURE_RAMPAR) && s.hits< s.hitsMax/2}}),
-                Game.rooms[r].find(FIND_STRUCTURES, {filter:(s)=>{return (s.structureType==STRUCTURE_WALL|| s.structureType==STRUCTURE_RAMPAR) && s.hits< s.hitsMax}})
+                Game.rooms[r].find(FIND_STRUCTURES, {filter:(s)=>{return (s.structureType==STRUCTURE_WALL|| s.structureType==STRUCTURE_RAMPART) && s.hits< s.hitsMax/2}}),
+                Game.rooms[r].find(FIND_STRUCTURES, {filter:(s)=>{return (s.structureType==STRUCTURE_WALL|| s.structureType==STRUCTURE_RAMPART) && s.hits< s.hitsMax}})
             ];
         }
         return out;
